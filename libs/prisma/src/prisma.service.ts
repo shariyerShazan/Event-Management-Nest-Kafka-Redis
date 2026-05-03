d23685bbe1547856fb75d05d8eefc936955c2242
shariyerShazan <shariyershazan1@gmail.com>
2026-05-03 23:17:23 +0600

    feat: prisma setup, kafka setup, prisma migrate and generate.. fix: some issue

---
+++ libs/prisma/src/prisma.service.ts
@@ -0,0 +1,36 @@
+import {
+  Injectable,
+  OnModuleInit,
+  OnModuleDestroy,
+  Logger,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  private logger = new Logger('PrismaService');
+
+  constructor() {
+    super({
+      log: [
+        { emit: 'stdout', level: 'query' },
+        { emit: 'stdout', level: 'info' },
+        { emit: 'stdout', level: 'warn' },
+        { emit: 'event', level: 'error' },
+      ],
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('Database connected successfully');
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    this.logger.log('Database disconnected');
+  }
+}
